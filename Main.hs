ea806b45e8772d8ae9bef94eae908499133549db
Niil Öhlin <niilohlin@gmail.com>
2017-06-22 11:07:33 +0200

    add keyboard listeners and model

--- Main.hs
+++ Main.hs
@@ -15,22 +15,32 @@ import qualified Helm.Sub as Sub
 import qualified Helm.Time as Time
 import Helm.Time (Time)
 import Linear.V2 (V2(V2))
+import Data.List.NonEmpty
+import qualified System.Random as Rand
 
 windowDims :: V2 Int
 windowDims = V2 800 600
 
-data Model = Model
+data Direction = North | South | West | East
+data Model = Model { snake :: NonEmpty (V2 Int), direction :: Direction , apple :: V2 Int, speed :: Double }
 
-data Action = DoNothing
+data Action = Move | ChangeDirection Direction | Die | NewApple Rand.StdGen | Eat | Wait
 
-initial :: (Model, Cmd SDLEngine a)
-initial = (Model, Cmd.none)
+initial :: (Model, Cmd SDLEngine Action)
+initial = (Model { snake = V2 0 0 :| [], apple = V2 0 0, direction = East, speed = 0.5 }, Cmd.execute Rand.newStdGen NewApple)
 
-update :: t -> t1 -> (Model, Cmd SDLEngine a)
+update :: t -> t1 -> (Model, Cmd SDLEngine Action)
 update model action = initial
 
 subscriptions :: Sub SDLEngine Action
-subscriptions = Sub.batch []
+subscriptions = Sub.batch [
+    Keyboard.presses $ \key -> ChangeDirection (case key of
+        Keyboard.UpKey -> North
+        Keyboard.DownKey -> South
+        Keyboard.RightKey -> East
+        Keyboard.LeftKey -> West
+        _ -> West
+    )]
 
 view :: Model -> Graphics SDLEngine
 view model = Graphics2D $  collage []
